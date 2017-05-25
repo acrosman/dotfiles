aa12cee53723071ad354165f87b23b48e0a7ecea
Aaron Crosman <acrosman@gmail.com>
2017-05-24 22:48:00 -0400

    Tweaks to MacOS.sh

--- macos.sh
+++ macos.sh
@@ -122,14 +122,14 @@ defaults write NSGlobalDomain NSAutomaticDashSubstitutionEnabled -bool false
 ###############################################################################
 
 # Disable hibernation (speeds up entering sleep mode)
-sudo pmset -a hibernatemode 0
+#sudo pmset -a hibernatemode 0
 
 # Remove the sleep image file to save disk space
-sudo rm /private/var/vm/sleepimage
+#sudo rm /private/var/vm/sleepimage
 # Create a zero-byte file instead…
-sudo touch /private/var/vm/sleepimage
+#sudo touch /private/var/vm/sleepimage
 # …and make sure it can’t be rewritten
-sudo chflags uchg /private/var/vm/sleepimage
+#sudo chflags uchg /private/var/vm/sleepimage
 
 # Disable the sudden motion sensor as it’s not useful for SSDs
 sudo pmset -a sms 0
@@ -564,52 +564,52 @@ sudo mdutil -E / > /dev/null
 # Only use UTF-8 in Terminal.app
 defaults write com.apple.terminal StringEncodings -array 4
 
-# Use a modified version of the Solarized Dark theme by default in Terminal.app
-osascript <<EOD
-
-tell application "Terminal"
-
-  local allOpenedWindows
-  local initialOpenedWindows
-  local windowID
-  set themeName to "Solarized Dark xterm-256color"
-
-  (* Store the IDs of all the open terminal windows. *)
-  set initialOpenedWindows to id of every window
-
-  (* Open the custom theme so that it gets added to the list
-     of available terminal themes (note: this will open two
-     additional terminal windows). *)
-  do shell script "open '$HOME/init/" & themeName & ".terminal'"
-
-  (* Wait a little bit to ensure that the custom theme is added. *)
-  delay 1
-
-  (* Set the custom theme as the default terminal theme. *)
-  set default settings to settings set themeName
-
-  (* Get the IDs of all the currently opened terminal windows. *)
-  set allOpenedWindows to id of every window
-
-  repeat with windowID in allOpenedWindows
-
-    (* Close the additional windows that were opened in order
-       to add the custom theme to the list of terminal themes. *)
-    if initialOpenedWindows does not contain windowID then
-      close (every window whose id is windowID)
-
-    (* Change the theme for the initial opened terminal windows
-       to remove the need to close them in order for the custom
-       theme to be applied. *)
-    else
-      set current settings of tabs of (every window whose id is windowID) to settings set themeName
-    end if
-
-  end repeat
-
-end tell
-
-EOD
+# # Use a modified version of the Solarized Dark theme by default in Terminal.app
+# osascript <<EOD
+#
+# tell application "Terminal"
+#
+#   local allOpenedWindows
+#   local initialOpenedWindows
+#   local windowID
+#   set themeName to "Solarized Dark xterm-256color"
+#
+#   (* Store the IDs of all the open terminal windows. *)
+#   set initialOpenedWindows to id of every window
+#
+#   (* Open the custom theme so that it gets added to the list
+#      of available terminal themes (note: this will open two
+#      additional terminal windows). *)
+#   do shell script "open '$HOME/init/" & themeName & ".terminal'"
+#
+#   (* Wait a little bit to ensure that the custom theme is added. *)
+#   delay 1
+#
+#   (* Set the custom theme as the default terminal theme. *)
+#   set default settings to settings set themeName
+#
+#   (* Get the IDs of all the currently opened terminal windows. *)
+#   set allOpenedWindows to id of every window
+#
+#   repeat with windowID in allOpenedWindows
+#
+#     (* Close the additional windows that were opened in order
+#        to add the custom theme to the list of terminal themes. *)
+#     if initialOpenedWindows does not contain windowID then
+#       close (every window whose id is windowID)
+#
+#     (* Change the theme for the initial opened terminal windows
+#        to remove the need to close them in order for the custom
+#        theme to be applied. *)
+#     else
+#       set current settings of tabs of (every window whose id is windowID) to settings set themeName
+#     end if
+#
+#   end repeat
+#
+# end tell
+#
+# EOD
 
 # Enable “focus follows mouse” for Terminal.app and all X11 apps
 # i.e. hover over a window and start typing in it without clicking first
@@ -621,7 +621,7 @@ defaults write org.x.X11 wm_ffm -bool true
 defaults write com.apple.terminal SecureKeyboardEntry -bool true
 
 # Install the Solarized Dark theme for iTerm
-open "${HOME}/init/Solarized Dark.itermcolors"
+#open "init/Solarized Dark.itermcolors"
 
 # Don’t display the annoying prompt when quitting iTerm
 defaults write com.googlecode.iterm2 PromptOnQuit -bool false
@@ -760,8 +760,7 @@ defaults write com.google.Chrome.canary PMPrintingExpandedStateForPrint2 -bool t
 
 for app in "Activity Monitor" "Address Book" "Calendar" "Contacts" "cfprefsd" \
   "Dock" "Finder" "Google Chrome" "Google Chrome Canary" "Mail" "Messages" \
-  "Opera" "Photos" "Safari" "SystemUIServer" "Terminal" \
-  "iCal"; do
+  "Opera" "Photos" "Safari" "SystemUIServer" "iCal"; do
   killall "${app}" &> /dev/null
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
